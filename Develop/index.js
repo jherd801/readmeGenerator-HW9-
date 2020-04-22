56f3d2638d7d34ca8e78694a315139499b15d0c4
jherd801 <jherd801@gmail.com>
2020-04-21 19:52:06 -0600

    Initial question array asking for title, description and TOC, plus the fsWriteFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,49 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const questions = ["Here's a question", "Here's another question"
 
 ];
 
-function writeToFile(fileName, data) {
-}
+console.log(questions);
 
-function init() {
+// Command line questions
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project called?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project"
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "Please enter a table of contents to describe your project's structure"
+    }
+]).then(function(data) {
 
-}
+    var fileName = data.title.toLowerCase().split(' ').join('') + ".json";
 
-init();
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
+});
+
+
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
